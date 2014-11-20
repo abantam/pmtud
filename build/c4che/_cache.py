36c73953508ceb0e8899bd4e8662e271d22f6a9c
Shimizu Takeshi <abantam.9@gmail.com>
2014-11-20 17:53:52 +0900

    2014-11-20-1

--- build/c4che/_cache.py
+++ build/c4che/_cache.py
@@ -3,11 +3,11 @@ AR = '/usr/bin/ar'
 ARFLAGS = 'rcs'
 BINDIR = '/usr/local/bin'
 BRITE = True
-BUILD_PROFILE = 'debug'
-BUILD_SUFFIX = '-debug'
+BUILD_PROFILE = 'optimized'
+BUILD_SUFFIX = '-optimized'
 CC = ['/usr/bin/gcc']
-CCDEFINES = ['_DEBUG']
-CCFLAGS = ['-O0', '-ggdb', '-g3', '-Wall', '-Werror', '-O0', '-ggdb', '-g3', '-Wall', '-Werror', '-Wno-error=deprecated-declarations', '-fstrict-aliasing', '-Wstrict-aliasing']
+CCDEFINES = []
+CCFLAGS = ['-O3', '-g', '-Wall', '-Werror', '-O3', '-g', '-Wall', '-Werror', '-march=native', '-fstrict-overflow', '-Wstrict-overflow=5', '-Wno-error=deprecated-declarations', '-fstrict-aliasing', '-Wstrict-aliasing']
 CCFLAGS_PTHREAD = '-pthread'
 CCFLAGS_PYEXT = ['-fvisibility=hidden']
 CCLNK_SRC_F = []
@@ -18,6 +18,7 @@ CC_TGT_F = ['-c', '-o']
 CC_VERSION = ('4', '8', '2')
 CFLAGS_GTK2 = ['-pthread']
 CFLAGS_MACBUNDLE = ['-fPIC']
+CFLAGS_MPI = ['-fstack-protector']
 CFLAGS_PYEMBED = ['-fno-strict-aliasing', '-fwrapv', '-fstack-protector', '-fno-strict-aliasing']
 CFLAGS_PYEXT = ['-pthread', '-fno-strict-aliasing', '-fwrapv', '-fstack-protector', '-fno-strict-aliasing', '-fwrapv', '-fstack-protector', '-fno-strict-aliasing']
 CFLAGS_cshlib = ['-fPIC']
@@ -27,10 +28,11 @@ CPPPATH = ['/home/shimizu/ns-allinone-3.21/BRITE', '/home/shimizu/ns-allinone-3.
 CPPPATH_BRITE = ['/home/shimizu/ns-allinone-3.21/BRITE', '/home/shimizu/ns-allinone-3.21/BRITE/Models']
 CPPPATH_ST = '-I%s'
 CXX = ['/usr/bin/g++']
-CXXDEFINES = ['_DEBUG', 'NS3_BRITE', 'NS3_BRITE']
-CXXFLAGS = ['-O0', '-ggdb', '-g3', '-Wall', '-Werror', '-Wno-error=deprecated-declarations', '-fstrict-aliasing', '-Wstrict-aliasing']
+CXXDEFINES = ['NS3_BRITE', 'NS3_BRITE']
+CXXFLAGS = ['-O3', '-g', '-Wall', '-Werror', '-march=native', '-fstrict-overflow', '-Wstrict-overflow=5', '-Wno-error=deprecated-declarations', '-fstrict-aliasing', '-Wstrict-aliasing']
 CXXFLAGS_GTK2 = ['-pthread']
 CXXFLAGS_MACBUNDLE = ['-fPIC']
+CXXFLAGS_MPI = ['-fstack-protector']
 CXXFLAGS_PTHREAD = '-pthread'
 CXXFLAGS_PYEMBED = ['-fno-strict-aliasing', '-fwrapv', '-fstack-protector', '-fno-strict-aliasing']
 CXXFLAGS_PYEXT = ['-pthread', '-fno-strict-aliasing', '-fwrapv', '-fstack-protector', '-fno-strict-aliasing', '-fwrapv', '-fstack-protector', '-fno-strict-aliasing', '-fvisibility=hidden', '-Wno-array-bounds']
@@ -42,7 +44,8 @@ CXX_SRC_F = []
 CXX_TGT_F = ['-c', '-o']
 DATADIR = '/usr/local/share'
 DATAROOTDIR = '/usr/local/share'
-DEFINES = ['NS3_ASSERT_ENABLE', 'NS3_LOG_ENABLE', 'HAVE_SYS_IOCTL_H=1', 'HAVE_IF_NETS_H=1', 'HAVE_NET_ETHERNET_H=1', 'HAVE_PACKET_H=1', 'HAVE_IF_TUN_H=1', 'HAVE_GSL=1']
+DEFINES = ['HAVE_SYS_IOCTL_H=1', 'HAVE_IF_NETS_H=1', 'HAVE_NET_ETHERNET_H=1', 'HAVE_PACKET_H=1', 'HAVE_MPI=1', 'HAVE_IF_TUN_H=1', 'HAVE_GSL=1']
+DEFINES_MPI = ['NS3_MPICH', 'NS3_MPI']
 DEFINES_NSCLICK = ['NS3_CLICK']
 DEFINES_PYEMBED = ['NDEBUG', '_FORTIFY_SOURCE=2']
 DEFINES_PYEXT = ['NDEBUG', '_FORTIFY_SOURCE=2', 'NDEBUG', '_FORTIFY_SOURCE=2']
@@ -61,6 +64,7 @@ ENABLE_FDNETDEV = True
 ENABLE_GSL = ' -lgsl -lgslcblas -lm  \n'
 ENABLE_GTK2 = '-pthread -I/usr/include/gtk-2.0 -I/usr/lib/x86_64-linux-gnu/gtk-2.0/include -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/pango-1.0 -I/usr/include/gio-unix-2.0/ -I/usr/include/freetype2 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/libpng12 -I/usr/include/harfbuzz  -lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 -lgio-2.0 -lpangoft2-1.0 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lcairo -lpango-1.0 -lfontconfig -lgobject-2.0 -lglib-2.0 -lfreetype  \n'
 ENABLE_LIBXML2 = '-I/usr/include/libxml2  -lxml2  \n'
+ENABLE_MPI = True
 ENABLE_NSC = False
 ENABLE_PYTHON_BINDINGS = True
 ENABLE_PYTHON_SCANNING = True
@@ -80,6 +84,7 @@ INCLUDEDIR = '/usr/local/include'
 INCLUDES_BRITE = '/home/shimizu/ns-allinone-3.21/BRITE'
 INCLUDES_GTK2 = ['/usr/include/gtk-2.0', '/usr/lib/x86_64-linux-gnu/gtk-2.0/include', '/usr/include/atk-1.0', '/usr/include/cairo', '/usr/include/gdk-pixbuf-2.0', '/usr/include/pango-1.0', '/usr/include/gio-unix-2.0/', '/usr/include/freetype2', '/usr/include/glib-2.0', '/usr/lib/x86_64-linux-gnu/glib-2.0/include', '/usr/include/pixman-1', '/usr/include/libpng12', '/usr/include/harfbuzz']
 INCLUDES_LIBXML2 = ['/usr/include/libxml2']
+INCLUDES_MPI = ['/usr/include/mpich']
 INCLUDES_NSCLICK = ['/home/shimizu/ns-allinone-3.21/click/include']
 INCLUDES_PYEMBED = ['/usr/include/python2.7', '/usr/include/x86_64-linux-gnu/python2.7']
 INCLUDES_PYEXT = ['/usr/include/python2.7', '/usr/include/x86_64-linux-gnu/python2.7']
@@ -88,6 +93,7 @@ INT64X64_USE_128 = 1
 LIBDIR = '/usr/local/lib'
 LIBEXECDIR = '/usr/local/libexec'
 LIBPATH_BRITE = ['/home/shimizu/ns-allinone-3.21/BRITE']
+LIBPATH_MPI = ['/usr/lib/x86_64-linux-gnu']
 LIBPATH_NSCLICK = ['/home/shimizu/ns-allinone-3.21/click/ns']
 LIBPATH_PYEMBED = ['/usr/lib']
 LIBPATH_PYEXT = ['/usr/lib']
@@ -99,6 +105,7 @@ LIB_DL = ['dl']
 LIB_GSL = ['gsl', 'gslcblas', 'm']
 LIB_GTK2 = ['gtk-x11-2.0', 'gdk-x11-2.0', 'atk-1.0', 'gio-2.0', 'pangoft2-1.0', 'pangocairo-1.0', 'gdk_pixbuf-2.0', 'cairo', 'pango-1.0', 'fontconfig', 'gobject-2.0', 'glib-2.0', 'freetype']
 LIB_LIBXML2 = ['xml2']
+LIB_MPI = ['mpichcxx', 'mpich', 'opa', 'mpl', 'rt', 'cr', 'pthread']
 LIB_NSCLICK = ['nsclick']
 LIB_PYEMBED = ['python2.7']
 LIB_PYEXT = ['python2.7']
@@ -107,6 +114,7 @@ LIB_RT = ['rt']
 LIB_ST = '-l%s'
 LINKFLAGS_GTK2 = ['-pthread']
 LINKFLAGS_MACBUNDLE = ['-bundle', '-undefined', 'dynamic_lookup']
+LINKFLAGS_MPI = ['-Wl,-Bsymbolic-functions', '-Wl,-z,relro']
 LINKFLAGS_PTHREAD = '-pthread'
 LINKFLAGS_PYEMBED = ['-Wl,-Bsymbolic-functions', '-Wl,-z,relro']
 LINKFLAGS_PYEXT = ['-Wl,-Bsymbolic-functions', '-Wl,-z,relro', '-pthread', '-Wl,-O1', '-Wl,-Bsymbolic-functions', '-Wl,-Bsymbolic-functions', '-Wl,-z,relro']
@@ -124,7 +132,7 @@ NS3_ENABLED_MODULES = ['ns3-antenna', 'ns3-aodv', 'ns3-applications', 'ns3-bridg
 NS3_EXECUTABLE_PATH = ['/home/shimizu/ns-allinone-3.21/ns-3.21/build/src/emu', '/home/shimizu/ns-allinone-3.21/ns-3.21/build/src/fd-net-device', '/home/shimizu/ns-allinone-3.21/ns-3.21/build/src/tap-bridge']
 NS3_MODULES = ['ns3-antenna', 'ns3-aodv', 'ns3-applications', 'ns3-bridge', 'ns3-brite', 'ns3-buildings', 'ns3-click', 'ns3-config-store', 'ns3-core', 'ns3-csma', 'ns3-csma-layout', 'ns3-dsdv', 'ns3-dsr', 'ns3-emu', 'ns3-energy', 'ns3-fd-net-device', 'ns3-flow-monitor', 'ns3-internet', 'ns3-lr-wpan', 'ns3-lte', 'ns3-mesh', 'ns3-mobility', 'ns3-mpi', 'ns3-netanim', 'ns3-network', 'ns3-nix-vector-routing', 'ns3-olsr', 'ns3-point-to-point', 'ns3-point-to-point-layout', 'ns3-propagation', 'ns3-sixlowpan', 'ns3-spectrum', 'ns3-stats', 'ns3-tap-bridge', 'ns3-test', 'ns3-topology-read', 'ns3-uan', 'ns3-virtual-net-device', 'ns3-visualizer', 'ns3-wave', 'ns3-wifi', 'ns3-wimax']
 NS3_MODULE_PATH = ['/usr/lib/gcc/x86_64-linux-gnu/4.8', '/home/shimizu/ns-allinone-3.21/ns-3.21/build', '/home/shimizu/ns-allinone-3.21/BRITE', '/home/shimizu/ns-allinone-3.21/click/lib', '/home/shimizu/ns-allinone-3.21/click/ns']
-NS3_OPTIONAL_FEATURES = [('python', 'Python Bindings', True, None), ('pygccxml', 'Python API Scanning Support', True, None), ('brite', 'BRITE Integration', True, 'BRITE library not found'), ('nsclick', 'NS-3 Click Integration', True, 'nsclick library not found'), ('GtkConfigStore', 'GtkConfigStore', '-pthread -I/usr/include/gtk-2.0 -I/usr/lib/x86_64-linux-gnu/gtk-2.0/include -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/pango-1.0 -I/usr/include/gio-unix-2.0/ -I/usr/include/freetype2 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/libpng12 -I/usr/include/harfbuzz  -lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 -lgio-2.0 -lpangoft2-1.0 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lcairo -lpango-1.0 -lfontconfig -lgobject-2.0 -lglib-2.0 -lfreetype  \n', "library 'gtk+-2.0 >= 2.12' not found"), ('XmlIo', 'XmlIo', '-I/usr/include/libxml2  -lxml2  \n', "library 'libxml-2.0 >= 2.7' not found"), ('Threading', 'Threading Primitives', True, '<pthread.h> include not detected'), ('RealTime', 'Real Time Simulator', True, 'threading not enabled'), ('EmuNetDevice', 'Emulated Net Device', True, '<netpacket/packet.h> include not detected'), ('FdNetDevice', 'File descriptor NetDevice', True, 'FdNetDevice module enabled'), ('TapFdNetDevice', 'Tap FdNetDevice', True, 'Tap support enabled'), ('EmuFdNetDevice', 'Emulation FdNetDevice', True, 'Emulation support enabled'), ('PlanetLabFdNetDevice', 'PlanetLab FdNetDevice', False, 'PlanetLab operating system not detected (see option --force-planetlab)'), ('nsc', 'Network Simulation Cradle', False, 'NSC not found (see option --with-nsc)'), ('mpi', 'MPI Support', False, 'option --enable-mpi not selected'), ('openflow', 'NS-3 OpenFlow Integration', False, 'Required boost libraries not found'), ('SqliteDataOutput', 'SQlite stats data output', [], "library 'sqlite3' not found"), ('TapBridge', 'Tap Bridge', True, '<linux/if_tun.h> include not detected'), ('PyViz', 'PyViz visualizer', True, None), ('ENABLE_SUDO', 'Use sudo to set suid bit', True, 'because we like it'), ('ENABLE_TESTS', 'Build tests', True, 'option --enable-tests selected'), ('ENABLE_EXAMPLES', 'Build examples', True, 'option --enable-examples selected'), ('GSL', 'GNU Scientific Library (GSL)', ' -lgsl -lgslcblas -lm  \n', 'GSL not found')]
+NS3_OPTIONAL_FEATURES = [('python', 'Python Bindings', True, None), ('pygccxml', 'Python API Scanning Support', True, None), ('brite', 'BRITE Integration', True, 'BRITE library not found'), ('nsclick', 'NS-3 Click Integration', True, 'nsclick library not found'), ('GtkConfigStore', 'GtkConfigStore', '-pthread -I/usr/include/gtk-2.0 -I/usr/lib/x86_64-linux-gnu/gtk-2.0/include -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/pango-1.0 -I/usr/include/gio-unix-2.0/ -I/usr/include/freetype2 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/libpng12 -I/usr/include/harfbuzz  -lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 -lgio-2.0 -lpangoft2-1.0 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lcairo -lpango-1.0 -lfontconfig -lgobject-2.0 -lglib-2.0 -lfreetype  \n', "library 'gtk+-2.0 >= 2.12' not found"), ('XmlIo', 'XmlIo', '-I/usr/include/libxml2  -lxml2  \n', "library 'libxml-2.0 >= 2.7' not found"), ('Threading', 'Threading Primitives', True, '<pthread.h> include not detected'), ('RealTime', 'Real Time Simulator', True, 'threading not enabled'), ('EmuNetDevice', 'Emulated Net Device', True, '<netpacket/packet.h> include not detected'), ('FdNetDevice', 'File descriptor NetDevice', True, 'FdNetDevice module enabled'), ('TapFdNetDevice', 'Tap FdNetDevice', True, 'Tap support enabled'), ('EmuFdNetDevice', 'Emulation FdNetDevice', True, 'Emulation support enabled'), ('PlanetLabFdNetDevice', 'PlanetLab FdNetDevice', False, 'PlanetLab operating system not detected (see option --force-planetlab)'), ('nsc', 'Network Simulation Cradle', False, 'NSC not found (see option --with-nsc)'), ('mpi', 'MPI Support', True, ''), ('openflow', 'NS-3 OpenFlow Integration', False, 'Required boost libraries not found'), ('SqliteDataOutput', 'SQlite stats data output', [], "library 'sqlite3' not found"), ('TapBridge', 'Tap Bridge', True, '<linux/if_tun.h> include not detected'), ('PyViz', 'PyViz visualizer', True, None), ('ENABLE_SUDO', 'Use sudo to set suid bit', True, 'because we like it'), ('ENABLE_TESTS', 'Build tests', True, 'option --enable-tests selected'), ('ENABLE_EXAMPLES', 'Build examples', True, 'option --enable-examples selected'), ('GSL', 'GNU Scientific Library (GSL)', ' -lgsl -lgslcblas -lm  \n', 'GSL not found')]
 NSCLICK = True
 OLDINCLUDEDIR = '/usr/include'
 PACKAGE = 'ns'
@@ -171,6 +179,6 @@ cstlib_PATTERN = 'lib%s.a'
 cxxprogram_PATTERN = '%s'
 cxxshlib_PATTERN = 'lib%s.so'
 cxxstlib_PATTERN = 'lib%s.a'
-define_key = ['HAVE_PACKET_H', 'HAVE_SYS_IOCTL_H', 'HAVE_IF_NETS_H', 'HAVE_NET_ETHERNET_H', 'HAVE_IF_TUN_H', 'HAVE_GSL']
+define_key = ['HAVE_PACKET_H', 'HAVE_SYS_IOCTL_H', 'HAVE_IF_NETS_H', 'HAVE_NET_ETHERNET_H', 'HAVE_IF_TUN_H', 'HAVE_MPI', 'HAVE_GSL']
 macbundle_PATTERN = '%s.bundle'
 pyext_PATTERN = '%s.so'
--- pmtud/test-topology-helper2.cc
+++ pmtud/test-topology-helper2.cc
@@ -1,24 +1,33 @@
 /*
-スター型トポロジーの自動生成
+グリッド型トポロジーの自動生成例
 */
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
 
 #include "ns3/point-to-point-layout-module.h"
 
+using namespace ns3;
+
 int main(int argc, char *argv[]) {
-  CommandLine cmd;
-  cmd.Parse(argc, argv);
-
-  uint32_t numSpokes = 12;//中央ノードを含む全ノード数
-
-  PointTpPointHelper p2p;//ノード接続用ヘルパーのインスタンス
-  PointToPointStarHelper star_net(numSpokes, p2p);
-
-  InternetStackHelper stack;
-  star_net.InstallStack(stack);
-
-  star_net.AssignIpv4Addresses(Ipv4AddressHelper("192.168.1.0", "255.255.255.0"));
-  
-  Simulator::Run();
-  Simulator::Destroy();
-  return 0;
+	CommandLine cmd;
+	cmd.Parse(argc, argv);
+	
+	uint32_t nRows = 8;//グリッドの行数
+	uint32_t nCols = 8;//グリッドの列数
+	
+	PointToPointHelper p2p;
+	PointToPointGridHelper grid_net(nRows, nCols, p2p);
+	
+	InternetStackHelper stack;
+	grid_net.InstallStack(stack);
+	
+	grid_net.AssignIpv4Addresses(Ipv4AddressHelper("10.1.1.0", "255.255.255.0"), Ipv4AddressHelper("10.2.1.0", "255.255.255.0"));
+	
+	Simulator::Run();
+	Simulator::Destroy();
+	
+	return 0;
 }
